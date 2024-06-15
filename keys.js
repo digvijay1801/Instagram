b7f8484cf9541f6028eb3abdebcbe2d84f40cfad
digvijay1801 <digvijayraut10@gmail.com>
2024-06-15 23:43:39 +0530

    Your commit message

---
+++ keys.js
@@ -0,0 +1,14 @@
+module.exports = {
+    mongoUrl:"mongodb+srv://admin:GFqDTAs8vTNQaymO@hosted-insta.nmgxhyo.mongodb.net/?retryWrites=true&w=majority&appName=hosted-insta" ,
+    Jwt_secret: "faslkfocvneofu"
+
+};
+
+
+//  "mongodb+srv://FlyingPanda730:UgpngUX6DVAx1Xq0@cluster0.ormku05.mongodb.net/?retryWrites=true&w=majority"
+
+//  mongodb+srv://digvijayraut10:7266nui6aa1VS78i@hosted-insta.nmgxhyo.mongodb.net/?retryWrites=true&w=majority&appName=hosted-insta
+
+// GFqDTAs8vTNQaymO
+
+// mongodb+srv://admin:GFqDTAs8vTNQaymO@hosted-insta.nmgxhyo.mongodb.net/?retryWrites=true&w=majority&appName=hosted-insta
